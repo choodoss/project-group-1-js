ee4deba0dc52dde54f54623d82eb515fb8d59e14
Дмитрий Хоменко <dimkakhomenko@gmail.com>
2023-04-13 00:06:57 +0300

    comm

--- src/js/film-card.js
+++ src/js/film-card.js
@@ -2,7 +2,7 @@ export function filmCardMacker(arr) {
     return arr.map(item => {
         return `<li class="film">
     <a class="film-link" data-modal-open href="javascript:void(0)">
-        <img loading="lazy" width="280" height="398" src="https://image.tmdb.org/t/p/w500${item.poster_path}" alt="${item.title}film cover"
+        <img loading="lazy" width="280" height="398" src="https://image.tmdb.org/t/p/w400${item.poster_path}" alt="${item.title}film cover"
             class="film__img">
             <div class="film-body">
                 <p class="film__name">${item.title}</p>
@@ -13,3 +13,5 @@ export function filmCardMacker(arr) {
     }
     ).join('')
 }
+
+//https://image.tmdb.org/t/p/w400${item.poster_path} - потрібно переробити із параметрами.
