fcfcb4781813f44ed8e4e8947dbac46c8ae33e4c
Kirill Kutovuy <114417403+Kirill99k@users.noreply.github.com>
2023-04-13 22:46:53 +0300

    add local storage

---
+++ src/js/localStorageAPI.js
@@ -0,0 +1,26 @@
+export const localStorageAPI = {
+  save,
+  load,
+};
+
+function save(key, value) {
+  // зберігає значення в форматі з JSON
+  try {
+    const dataJSON = JSON.stringify(value);
+    localStorage.setItem(key, dataJSON);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function load(key) {
+  // завантажує (віддає) значення в форматі з JSON
+  try {
+    const dataJSON = localStorage.getItem(key);
+    return dataJSON === null
+      ? { watched: [], queue: [] }
+      : JSON.parse(dataJSON);
+  } catch (error) {
+    console.log(error);
+  }
+}
