e1d01a7394b896aaef1fad9e82d6764bbf7e31d9
Дмитрий Хоменко <dimkakhomenko@gmail.com>
2023-04-13 17:06:46 +0300

    fix

--- src/js/ApiRequest.js
+++ src/js/ApiRequest.js
@@ -1,7 +1,7 @@
-const popularFilm = ['movie/popular'];
-const searchMovie = 'search/movie';
-const movieDetails = 'movie/' //'movie/${ID}'
-const genre = ['genre/tv/list', 'genre/movie/list'];
+const popularFilm = ['movie/popular']; // тренди для початкового запиту
+const searchMovie = 'search/movie';// тпошук по назві фільму
+const movieDetails = 'movie/' //'movie/${ID}' // детальна інформація по фільму із ID Куди записувати ID фільму?
+const genre = ['genre/tv/list', 'genre/movie/list']; // Для створення масиву жанрів із ID
 
 export default {
     popularFilm,
