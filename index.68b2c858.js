c54b720e95b1adf995282cbaa57a5871d222c32d
choodoss <82578557+choodoss@users.noreply.github.com>
2023-04-14 16:05:04 +0000

    Deploying to gh-pages from @ choodoss/project-group-1-js@2417201072f1ca36076c93c2e40ba24be16e6161 🚀

---
+++ index.68b2c858.js
@@ -0,0 +1,2 @@
+!function(){var o=document.querySelector(".upward");o.addEventListener("click",(function(){window.scrollTo({top:0,behavior:"smooth"})})),window.addEventListener("scroll",(function(){var n=window.innerHeight;o.classList.toggle("active",window.scrollY>.5*n)}))}();
+//# sourceMappingURL=index.68b2c858.js.map
