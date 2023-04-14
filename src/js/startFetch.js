94589488a88d47d9d60eea1c0d5138f8c84b4ded
natalialashko <natasha0909761805@gmail.com>
2023-04-14 10:26:42 +0300

    Update startFeth.js

--- src/js/startFetch.js
+++ src/js/startFetch.js
@@ -9,4 +9,25 @@ getDataFilm(ApiRequest.popularFilm, { language: 'en-US' }).then(({ results }) =>
     return
 })
 
+const inputSearchEll = document.querySelector('.header-nav__input'); // посилання на інпут для вводу ключового слова для пошуку
+console.log('inputSearchEll----',inputSearchEll);
+const searchBtn = document.querySelector('.search__icon'); // посилання на кнопку для пошуку
+console.log('searchBtn', searchBtn);
 
+searchBtn.addEventListener('click', function (event) {
+    const querySearch = inputSearchEll.value.trim();
+    console.log('querySearch ---', querySearch, querySearch.length);
+    if (querySearch.length === 0) {
+       console.log("введіть текст для пошуку кінофільмів") 
+    }
+
+getDataFilm(ApiRequest.searchMovie, { query: querySearch }).then(({ results }) => {// запит по трендам + запит на вставку карток у films-list
+    console.log(results)
+     if (results.length === 0) {
+         console.log("кінофільмів згідно вашого запиту немає") 
+         return
+    }
+    filmList.innerHTML = filmCardMacker(results);
+    return
+})
+})
