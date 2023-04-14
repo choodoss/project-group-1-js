33a1e8642cc7583915a0a31d5852efe4df996b00
natalialashko <natasha0909761805@gmail.com>
2023-04-14 17:06:09 +0300

    fix input

--- src/js/startFetch.js
+++ src/js/startFetch.js
@@ -11,11 +11,15 @@ getDataFilm(ApiRequest.popularFilm, { language: 'en-US' }).then(({ results }) =>
 
 const inputSearchEll = document.querySelector('.header-nav__input'); // посилання на інпут для вводу ключового слова для пошуку
 console.log('inputSearchEll----',inputSearchEll);
-const searchBtn = document.querySelector('.search__icon'); // посилання на кнопку для пошуку
-console.log('searchBtn', searchBtn);
+// const searchBtn = document.querySelector('.search__icon'); // посилання на кнопку для пошуку
+// console.log('searchBtn', searchBtn);
 
-searchBtn.addEventListener('click', function (event) {
-    const querySearch = inputSearchEll.value.trim();
+let timeoutId;
+inputSearchEll.addEventListener('input', function() {
+  clearTimeout(timeoutId);
+  timeoutId = setTimeout(function() {
+    // викликати функцію, яку потрібно запустити
+       const querySearch = inputSearchEll.value.trim();
     console.log('querySearch ---', querySearch, querySearch.length);
     if (querySearch.length === 0) {
        console.log("введіть текст для пошуку кінофільмів") 
@@ -30,5 +34,28 @@ getDataFilm(ApiRequest.searchMovie, { query: querySearch }).then(({ results }) =
     filmList.innerHTML = filmCardMacker(results);
     return
 })
-})
+     
+      
+    console.log('Текст був введений більше ніж 2 секунди тому.');
+  }, 2000);
+}); 
+
+
+// searchBtn.addEventListener('click', function (event) {
+//     const querySearch = inputSearchEll.value.trim();
+//     console.log('querySearch ---', querySearch, querySearch.length);
+//     if (querySearch.length === 0) {
+//        console.log("введіть текст для пошуку кінофільмів") 
+//     }
+
+// getDataFilm(ApiRequest.searchMovie, { query: querySearch }).then(({ results }) => {// запит по трендам + запит на вставку карток у films-list
+//     console.log(results)
+//      if (results.length === 0) {
+//          console.log("кінофільмів згідно вашого запиту немає") 
+//          return
+//     }
+//     filmList.innerHTML = filmCardMacker(results);
+//     return
+// })
+// })
 
