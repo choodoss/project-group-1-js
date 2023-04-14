df069b48434a75c79aabc25f69a5a1b290e1e875
Дмитрий Хоменко <82578557+choodoss@users.noreply.github.com>
2023-04-14 21:51:11 +0300

    fix film-card

--- src/js/startFetch.js
+++ src/js/startFetch.js
@@ -4,37 +4,37 @@ const filmList = document.querySelector('.films-list') // галерея кар�
 import ApiRequest from './ApiRequest'
 
 getDataFilm(ApiRequest.popularFilm, { language: 'en-US' }).then(({ results }) => {// запит по трендам + запит на вставку карток у films-list
-    console.log(results)
-    filmList.innerHTML = filmCardMacker(results);
-    return
+  console.log(results)
+  filmList.innerHTML = filmCardMacker(results);
+  return
 })
 
 const inputSearchEll = document.querySelector('.header-nav__input'); // посилання на інпут для вводу ключового слова для пошуку
-console.log('inputSearchEll----',inputSearchEll);
+console.log('inputSearchEll----', inputSearchEll);
 
 
 let timeoutId;
-inputSearchEll.addEventListener('input', function() {
+inputSearchEll.addEventListener('input', function () {
   clearTimeout(timeoutId);
-  timeoutId = setTimeout(function() {
+  timeoutId = setTimeout(function () {
     // викликати функцію, яку потрібно запустити
-       const querySearch = inputSearchEll.value.trim();
+    const querySearch = inputSearchEll.value.trim();
     console.log('querySearch ---', querySearch, querySearch.length);
     if (querySearch.length === 0) {
-       console.log("введіть текст для пошуку кінофільмів") 
+      console.log("введіть текст для пошуку кінофільмів")
     }
 
-getDataFilm(ApiRequest.searchMovie, { query: querySearch }).then(({ results }) => {// запит по трендам + запит на вставку карток у films-list
-    console.log(results)
-     if (results.length === 0) {
-         console.log("кінофільмів згідно вашого запиту немає") 
-         return
-    }
-    filmList.innerHTML = filmCardMacker(results);
-    return
-})
-     
-      
+    getDataFilm(ApiRequest.searchMovie, { query: querySearch }).then(({ results }) => {// запит по трендам + запит на вставку карток у films-list
+      console.log(results)
+      if (results.length === 0) {
+        console.log("кінофільмів згідно вашого запиту немає")
+        return
+      }
+      filmList.innerHTML = filmCardMacker(results);
+      return
+    })
+
+
     console.log('Текст був введений більше ніж 2 секунди тому.');
   }, 2000);
 }); 
