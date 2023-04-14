9066dd3c3c813526fe37730b761ec727209a35bb
AShvab <shvabovska@gmail.com>
2023-04-14 13:48:07 +0300

    spinner first version

---
+++ src/js/spinner.js
@@ -0,0 +1,20 @@
+
+export default class Spinner {
+    constructor() {
+      this.overlay = document.querySelector('.spinner__overlay');
+      this.spinner = document.querySelector('.spinner');
+    }
+  
+    async enable() {
+      await new Promise(resolve => setTimeout(resolve, 0)); 
+      this.overlay.classList.remove('is-hidden');
+      this.spinner.classList.add('spinner--start');
+    }
+  
+    async disable() {
+      await new Promise(resolve => setTimeout(resolve, 0)); 
+      this.overlay.classList.add('is-hidden');
+      this.spinner.classList.remove('spinner--start');
+    }
+  }
+  
