acf3adcd7b3a2fe86bae0d8050687ee21c44f193
AShvab <shvabovska@gmail.com>
2023-04-17 23:30:35 +0300

    add spinner

--- src/js/getDataFilm.js
+++ src/js/getDataFilm.js
@@ -1,8 +1,13 @@
+import Spinner from "./spinner";
+
 const API_KEY = '936b81ad25b58d3805a3200306ab9278';
 const URL = "https://api.themoviedb.org/3/";
 
+const spinner = new Spinner();
+
 
 export function getDataFilm(param, searchParam = {}) { //функція запиту карток по параметру посилання і параметру
+    spinner.enable(); //Увімкнути спіннер
     const parameters = new URLSearchParams({
         api_key: API_KEY,
         language: 'en-US',
@@ -14,11 +19,13 @@ export function getDataFilm(param, searchParam = {}) { //функція запи
             if (!res.ok) {
                 throw new Error(response.status);
             }
+            spinner.disable(); //Вимкнути спіннер
             return res.json()
         })
 };
 
 export async function getDataFilmTrailer(param, searchParam = {}) { //функція запиту карток по параметру посилання і параметру
+    spinner.enable(); //Увімкнути спіннер
     const parameters = new URLSearchParams({
         api_key: API_KEY,
         language: 'en-US',
@@ -30,15 +37,18 @@ export async function getDataFilmTrailer(param, searchParam = {}) { //функц
             if (!res.ok) {
                 throw new Error(response.status);
             }
+            spinner.disable(); //Вимкнути спіннер
             return res.json()
         })
 };
 
 export async function fetchTrailer(id) {
+    spinner.enable(); //Увімкнути спіннер
     const trailerUrl = `${BASE_URL}movie/${id}/videos?api_key=${API_KEY}`;
     const response = await fetch(trailerUrl);
     if (!response.ok) {
         throw new Error(response.statusText);
     }
+    spinner.disable(); //Вимкнути спіннер
     return await response.json();
 }
