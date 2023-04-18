484a0a68e1ee0ac177646951e0134b862a8ca7db
Vallyblink <alabama.toj@gmail.com>
2023-04-18 13:54:21 +0300

    storage in wirking

--- src/js/Firebase-storage.js
+++ src/js/Firebase-storage.js
@@ -1,11 +1,23 @@
-import { collection, addDoc } from "firebase/firestore"
-import { FirebaseStorage } from "./Firebase-init"
+import { collection, addDoc } from "firebase/firestore";
+import { FirebaseStorage } from "./Firebase-init";
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
-const add = document.querySelector('#btn')
+const add = document.querySelector('.add-to-watched')
 console.log(add)
 add.addEventListener('click', addInfo)
 function addInfo (e){
     e.preventDefault()
+    console.log('yeap')
+    const saveMovie = (movieData) => {
+  FirebaseStorage.collection('movies').add(movieData)
+    .then(() => {
+      console.log('Фільм успішно додано до Firebase!');
+    })
+    .catch((error) => {
+      console.error('Помилка при додаванні фільму до Firebase:', error);
+    });
+};
     console.log ('you are here')
     try {
     const docRef = addDoc(collection(FirebaseStorage, "users"), {
@@ -18,3 +30,7 @@ function addInfo (e){
     console.error("Error adding document: ", e);
   }
 }  
+
+
+
+export { saveMovie };
