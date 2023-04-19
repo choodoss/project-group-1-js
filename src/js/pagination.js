715a0d70bae26e3daf8996fc1573022a8abdf910
Дмитрий Хоменко <dimkakhomenko@gmail.com>
2023-04-19 12:44:25 +0300

    fix

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -23,4 +23,32 @@ pagination.on('beforeMove', function (eventData) {
 });
 
 //зробити подвійний фетч.
-// 
+//
+
+
+
+// const pagination = new Pagination('pagination', options);
+// const page = pagination.getCurrentPage();
+
+// pagination.on('beforeMove', loadMorePopylarPhotos);
+
+// const loadMorePopylarPhotos = async event => {
+//   const currentPage = event.page;
+//   Loading.hourglass({
+//     clickToClose: true,
+//     svgSize: '200px',
+//     svgColor: '#ff6b01',
+//   });
+//   render.clear();
+//   const data = await request.popular(currentPage);
+//   const genres = await request.genres();
+//   render.print(data, genres, markup.gallery);
+//   Loading.remove();
+// };
+
+// pagination.reset(data.total_results);
+// pagination.off('beforeMove', loadMorePopylarPhotos);
+// pagination.off('beforeMove', loadMoreOueryPhotos);
+// pagination.on('beforeMove', loadMoreOueryPhotos);
+
+// pagination.reset(data.total_results);
